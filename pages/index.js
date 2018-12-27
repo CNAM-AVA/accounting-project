5840bbed173f8f98a7c188244de4b3d22ae7d18c
Antoine Plard <azertyzhd@gmail.com>
2018-12-27 19:11:33 +0100

    core: app navigation

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,28 @@
+import Head from 'next/head'
 import React from 'react'
+import AppNavigation from '../components/AppNavigation'
+import "../static/styles.scss"
 
 export default class Index extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            msg: "yes"
+        };
     }
 
     render() {
-	return(
-	    <div><h1>Adrien Vimond</h1></div>
-	);
+    	return(
+            <div>
+                <Head>
+                    <title>Comptabilité</title>
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width, minimum-scale=1, shrink-to-fit=no" />
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+                </Head>
+                <AppNavigation></AppNavigation>
+            </div>
+    	);
     }
 }
