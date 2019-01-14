391447962bf20997528df13bee5924ffce4cd7f0
BOUILLENNEC Valentin <bouillennecvalentin@europe.essilor.group>
2019-01-14 17:17:55 +0100

    amortissements in progress

--- pages/amortissements.js
+++ pages/amortissements.js
@@ -2,7 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import MainLayout from "../components/MainLayout";
 import { withStyles } from '@material-ui/core/styles';
-import { Grid, FormControlLabel, FormControl, FormLabel, RadioGroup, Radio, TextField, TableRow, Table, TableHead, TableCell, TableBody } from '@material-ui/core';
+import moment from 'moment';
+import { Grid, FormControlLabel, FormControl, FormLabel, RadioGroup, Radio, TextField, TableRow, Table, TableHead, TableCell, TableBody, Button } from '@material-ui/core';
 
 const styles = theme => ({
     root: {
@@ -31,22 +32,25 @@ const styles = theme => ({
 	}
 });
 
+moment.locale('fr');
+
 let id = 0;
 function createData(dateDebut, dateFin, calcul, montant) {
   id += 1;
   return { id, dateDebut, dateFin, calcul, montant };
 }
 
-const rows = [
-	createData('23/07/2018', '31/12/2018', '10 000 x (158/360) x 20%', '877,78 €'),
-	createData('01/01/2019', '31/12/2019', '10 000 x 20%', '2 000,00 €'),
-	createData('01/01/2020', '31/12/2020', '10 000 x 20%', '2 000,00 €'),
-	createData('01/01/2021', '31/12/2021', '10 000 x 20%', '2 000,00 €'),
-	createData('01/01/2022', '31/12/2022', '10 000 x 20%', '2 000,00 €'),
-	createData('01/01/2023', '22/07/2023', '10 000 x (202/360) x 20%', '2 000,00 €'),
-	createData('Total', '', '', '10 000,00 €'),
+
+// const rows = [
+// 	createData('23/07/2018', '31/12/2018', '10 000 x (158/360) x 20%', '877,78 €'),
+// 	createData('01/01/2019', '31/12/2019', '10 000 x 20%', '2 000,00 €'),
+// 	createData('01/01/2020', '31/12/2020', '10 000 x 20%', '2 000,00 €'),
+// 	createData('01/01/2021', '31/12/2021', '10 000 x 20%', '2 000,00 €'),
+// 	createData('01/01/2022', '31/12/2022', '10 000 x 20%', '2 000,00 €'),
+// 	createData('01/01/2023', '22/07/2023', '10 000 x (202/360) x 20%', '2 000,00 €'),
+// 	createData('Total', '', '', '10 000,00 €'),
 	
-];
+// ];
 
 class Amortissements extends React.Component {
 
@@ -55,20 +59,54 @@ class Amortissements extends React.Component {
 		this.state = {
 			msg: "yes",
 			value: "lineaire",
+			formControls: {
+				dateDebut: {
+					value: moment().format('YYYY-MM-DD')
+				},
+				dateFin: {
+					value: moment().format('YYYY-MM-DD')
+				},
+				montant: {
+					value: '0'
+				},
+			},
+			rows: [],			
 		};
 	}
 
 	handleChange = event => {
-		this.setState({ value: event.target.value });
+		const name = event.target.name;
+		const value = event.target.value;
+
+		this.setState({
+			formControls: {
+				[name]: value
+			}
+		});
+		console.log(this.state.formControls.dateDebut.value);
+		console.log(this.state.formControls.dateFin.value);
 	};
 
-	
+	handleSubmit = event => {
+		const name = event.target.name;
+		const value = event.target.value;
+		this.setState({ 
+			formControls: {
+				[name]: value
+			}
+		});
+		console.log(this.state.formControls.dateDebut.value);
+
+	};
 
 	render() {
 		const { classes } = this.props;
+		const rows = this.state.rows;
 
 		return(
 			<MainLayout>
+				{/* <p>{ this.state.formControls.dateDebut.value }</p>
+				<p>{ this.state.formControls.dateFin.value }</p> */}
 				<FormControl component="fieldset" className={classes.formControl}>
 					<FormLabel className={classes.formLabel}>Type d'amortissement</FormLabel>
 					<RadioGroup
@@ -78,19 +116,20 @@ class Amortissements extends React.Component {
 						value={this.state.value}
 						onChange={this.handleChange}
 					>
-						<FormControlLabel value="lineaire" control={<Radio />} label="Linéaire"></FormControlLabel>
-						<FormControlLabel value="variable" control={<Radio />} label="Variable"></FormControlLabel>
-						<FormControlLabel value="degressif" control={<Radio />} label="Dégressif"></FormControlLabel>
+						<FormControlLabel value="lineaire" control={<Radio color="primary"/>} label="Linéaire"></FormControlLabel>
+						<FormControlLabel value="variable" control={<Radio color="primary"/>} label="Variable"></FormControlLabel>
+						<FormControlLabel value="degressif" control={<Radio color="primary"/>} label="Dégressif"></FormControlLabel>
 					</RadioGroup>
 				</FormControl>
-				<form className={classes.container} noValidate autoComplete="off">
+				<form className={classes.container} noValidate autoComplete="off" onSubmit={this.handleSubmit}>
 					<FormControl component="fieldset" className={classes.formControl}>
 
 						<FormLabel className={classes.formLabel}>Calcul</FormLabel>
 						<TextField
 							id="dateDebut"
 							label="Date de début d'amortissement"
-							value={this.state.dateDebut}
+							name="dateDebut"
+							// value={this.state.formControls.dateDebut.value}
 							onChange={this.handleChange}
 							type="date"
 							className={classes.textField}
@@ -102,10 +141,11 @@ class Amortissements extends React.Component {
 						/>
 						<TextField
 							id="outlined-number"
-							label="Durée de vie du bien"
-							value={this.state.dureeVie}
+							label="Date de fin d'amortissement"
+							name="dateFin"
+							// value={this.state.formControls.dateFin.value}
 							onChange={this.handleChange}
-							type="number"
+							type="date"
 							className={classes.textField}
 							InputLabelProps={{
 								shrink: true,
@@ -116,7 +156,8 @@ class Amortissements extends React.Component {
 						<TextField
 							id="outlined-montant"
 							label="Montant du bien"
-							value={this.state.montant}
+							name="montant"
+							value={this.state.formControls.montant.value}
 							onChange={this.handleChange}
 							type="number"
 							className={classes.textField}
@@ -126,11 +167,14 @@ class Amortissements extends React.Component {
 							margin="normal"
 							variant="outlined"
 						/>
+						<Button variant="outlined" color="primary">
+							Générer le tableau d'amortissement
+						</Button>
 					</FormControl>
 				</form>
 				<FormControl component="fieldset" className={classes.formControl}>
 					<FormLabel  className={classes.formLabel}>Tableau d'amortissement</FormLabel>
-					<Table className={classes.table}>
+					<Table>
 						<TableHead>
 						<TableRow>
 							<TableCell>Date de début</TableCell>
